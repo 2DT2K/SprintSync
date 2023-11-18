fe4e7df940e33986f8ec5623bbe08b69b481f1f9
Erioldeth <crystaleye005@gmail.com>
2023-11-18 21:44:44 +0700

    adding missing dependencies after merging

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,7 +55,8 @@ android {
 dependencies {
 
 	implementation("androidx.core:core-ktx:1.12.0")
-	implementation("androidx.activity:activity-compose:1.8.0")
+	implementation("androidx.core:core-splashscreen:1.1.0-alpha02")
+	implementation("androidx.activity:activity-compose:1.8.1")
 	implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
 	implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.2")
 	implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
