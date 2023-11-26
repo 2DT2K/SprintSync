a8fcee01277e09d3a1880b77424264cec6b6bd13
Trần Chiến Thắng <ctb0k33@gmail.com>
2023-11-26 22:01:03 +0700

    merged with main

--- app/src/main/java/com/sprintsync/data/dtos/response/TaskResDto.kt
+++ app/src/main/java/com/sprintsync/data/dtos/response/TaskResDto.kt
@@ -1,21 +1,22 @@
 package com.sprintsync.data.dtos.response
-
+import com.sprintsync.data.dtos.AttachmentDto
 import com.sprintsync.data.dtos.MemberDto
 import com.sprintsync.data.dtos.TeamDto
 
 data class TaskResDto(
-	val id: String? = null,
-	val name: String,
-	val description: String? = null,
-	val sprint: String,
-	val team: TeamDto,
-	val assignor: MemberDto,
-	val assignees: List<MemberDto>,
-	val parentTask: String? = null,
-	val attachments: List<String>? = null,
-	val statusIndex: Int,
-	val deadline: String? = null,
-	val point: Int = 0,
-	val comments: List<String>? = null,
-	val labels: List<String>? = null
+    val id: String? = null,
+    val name: String,
+    val description: String? = null,
+    val sprint: String,
+    val team: TeamDto,
+    val assignor: MemberDto,
+    val assignees: List<MemberDto>,
+    val parentTask: String? = null,
+    val subTasks: List<TaskResDto>? = null,
+    val attachments: List<AttachmentDto>? = null,
+    val statusIndex: Int,
+    val deadline: String? = null,
+    val point: Int = 0,
+    val comments: List<CommentResDto>? = null,
+    val labels: List<String>? = null
 )
